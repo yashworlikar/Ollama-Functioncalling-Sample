49f399c24e69c28b6e64495ac4078a6acff303f7
Yash W <yash.w@systenics.net>
2024-12-09 11:26:08 +0530

    Add project files.

---
+++ Plugins.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.SemanticKernel;
+using System.ComponentModel;
+
+namespace OllamaSample
+{
+    /// <summary>
+    /// Simple plugin that just returns the time.
+    /// </summary>
+    public class MyTimePlugin
+    {
+        [KernelFunction, Description("Get the current time")]
+        public DateTimeOffset Time() => DateTimeOffset.Now;
+    }
+
+
+    /// <summary>
+    /// Class that represents a controllable light bulb.
+    /// </summary>
+    [Description("Represents a light bulb")]
+    public class MyLightPlugin(bool turnedOn = false)
+    {
+        private bool _turnedOn = turnedOn;
+
+        [KernelFunction, Description("Returns whether this light is on")]
+        public bool IsTurnedOn() => _turnedOn;
+
+        [KernelFunction, Description("Turn on this light")]
+        public void TurnOn() => _turnedOn = true;
+
+        [KernelFunction, Description("Turn off this light")]
+        public void TurnOff() => _turnedOn = false;
+    }
+
+    /// <summary>
+    /// Class that represents a controllable alarm.
+    /// </summary>
+    public class MyAlarmPlugin
+    {
+        private string _hour;
+
+        public MyAlarmPlugin(string providedHour)
+        {
+            this._hour = providedHour;
+        }
+
+        [KernelFunction, Description("Sets an alarm at the provided time")]
+        public string SetAlarm(string time)
+        {
+            this._hour = time;
+            return GetCurrentAlarm();
+        }
+
+        [KernelFunction, Description("Get current alarm set")]
+        public string GetCurrentAlarm()
+        {
+            return $"Alarm set for {_hour}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.Ollama;
+using OllamaSample;
+
+var builder = Kernel.CreateBuilder();
+var modelId = "llama3.2";
+var endpoint = new Uri("http://localhost:11434");
+
+builder.Services.AddOllamaChatCompletion(modelId, endpoint);
+
+builder.Plugins
+    .AddFromType<MyTimePlugin>()
+    .AddFromObject(new MyLightPlugin(turnedOn: true))
+    .AddFromObject(new MyAlarmPlugin("11"));
+
+builder.Services.AddLogging(x => { x.AddConsole(); x.SetMinimumLevel(LogLevel.Information); });
+var kernel = builder.Build();
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+var settings = new OllamaPromptExecutionSettings { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() };
+
+Console.WriteLine("""
+    Ask questions or give instructions to the copilot such as:
+    - Change the alarm to 8
+    - What is the current alarm set?
+    - Is the light on?
+    - Turn the light off please.
+    - Set an alarm for 6:00 am.
+    """);
+
+Console.Write("> ");
+
+string? input = null;
+while ((input = Console.ReadLine()) is not null)
+{
+    Console.WriteLine();
+
+    try
+    {
+        ChatMessageContent chatResult = await chatCompletionService.GetChatMessageContentAsync(input, settings, kernel);
+        Console.Write($"\n>>> Result: {chatResult}\n\n> ");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}\n\n> ");
+    }
+}
+
+
